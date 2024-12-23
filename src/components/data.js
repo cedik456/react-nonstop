154dcd1822fe24c218f32d8f52c9499ff3220f56
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2024-12-23 15:19:14 +0800

    First Commit

---
+++ src/components/data.js
@@ -0,0 +1,24 @@
+const dummyData = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+  },
+  {
+    id: 2,
+    question: "What is 2 + 2?",
+    answer: "4",
+  },
+  {
+    id: 3,
+    question: "Who wrote 'To Kill a Mockingbird'?",
+    answer: "Harper Lee",
+  },
+  {
+    id: 4,
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+  },
+];
+
+export default dummyData;
